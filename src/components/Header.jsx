311636c0dd0ff9377ecab0adc37ea6599a4293b7
kshreeya2006 <kshreeya2006@gmail.com>
2025-05-23 20:04:11 +0530

    changed color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import "./Header.css";
 export default function Header() {
   return (
      <div className="App-Header-Row">
-      <h1 style={{backgroundColor:'blue',color:'blue'}}>React Lessons</h1>
+      <h1 style={{backgroundColor:'blue',color:'black'}}>React Lessons</h1>
       <Link to="/">App1</Link>-<Link to="/app2">App2</Link>
     </div>
   );
