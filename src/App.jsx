576f06efe54b83bb2ab979076c085994b579d3f2
kshreeya2006 <kshreeya2006@gmail.com>
2025-05-24 20:03:20 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import App2 from "./components/App2";
 import App3 from "./components/App3";
 import App4 from "./components/App4";
 import App5 from "./components/App5";
+import App6 from "./components/App6";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
