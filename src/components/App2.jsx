d63b8fcff1aa0729ef473329089370c16800c86a
kshreeya2006 <kshreeya2006@gmail.com>
2025-05-24 19:44:28 +0530

    Added new components

--- src/components/App2.jsx
+++ src/components/App2.jsx
@@ -1,22 +1,24 @@
-import "./App2.css"; 
+import "./App2.css";
+import Product from "./Product";
 export default function App2() {
   const products = [
-    { id: 1, name: "Product 1", price: 50 },
-    { id: 2, name: "Product 2", price: 60 },
-    { id: 3, name: "Product 3", price: 45 },
+    { id: 1, name: "Product 1", price: 45 },
+    { id: 2, name: "Product 2", price: 50 },
+    { id: 3, name: "Product 3", price: 75 },
   ];
   return (
     <div>
       <h1>This is App2 component</h1>
+      <h2>Working with Arrays and Objects,Applying Styles</h2>
       <div className="App-Product-Container">
         {products.map((value) => (
-          <div className="App-Product-Box" key={value.id}>
-            <h3>{value.name}</h3>
-            <h4>{value.price}</h4>
-            <p><button>Add to Cart</button></p>
+          <div className="App-Product-Box">
+            <h2>{value.name}</h2>
+            <h3>{value.price}</h3>
+            <button>Add to Cart</button>
           </div>
         ))}
       </div>
     </div>
   );
-}    
+}
